d9d5bacc39599f7940939fec6c85b0515cb69957
Chase Martin <chasemartin88@gmail.com>
2018-08-27 08:37:26 -0400

    added fog

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.4.4'
 
 
+gem 'figaro'
 gem 'carrierwave'
 gem 'faker'
 gem 'fog'
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,12 @@
-# config/initiliazers/carrierwave.rb
-
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider              => 'AWS',
-    :aws_access_key_id     => "#{ENV['AWS_ACCESS_KEY_ID']}",
-    :aws_secret_access_key => "#{ENV['AWS_SECRET_ACCESS_KEY']}",
-    :region                => "#{ENV['S3_REGION']}",
-    :path_style            => true
+    provider: 'AWS',
+    aws_access_key_id: "#{ENV['AWS_ACCESS_KEY_ID']}",
+    aws_secret_key: "#{ENV['AWS_SECRET_KEY_ID']}",
+    region: "#{ENV['S3_REGION']}",
+    path_style: true
   }
 
-  config.fog_directory =  "#{ENV['S3_BUCKET_NAME']}"
-  config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
+  config.fog_directory = "#{ENV['S3_BUCKET']}"
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
 end
